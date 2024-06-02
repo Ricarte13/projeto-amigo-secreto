075e15927ee70d59061b25d62c9a139b751327c7
Ricarte13 <viniciusdiv@hotmail.com>
2024-06-02 14:10:26 -0300

    final project

--- js/app.js
+++ js/app.js
@@ -1,76 +1,64 @@
 let amigos = [];
 
-function adicionar(){
+function adicionar() {
     let nomeAmigo = document.getElementById('nome-amigo');
+    if (nomeAmigo.value == '') {
+        alert('Informe um nome válido!')
+        return;
+    }
+
+    if (amigos.includes(nomeAmigo.value)) {
+        alert("Nome já existente, digite outro nome!");
+        return;
+    }
+
     let listaAmigo = document.getElementById('lista-amigos');
+    
     amigos.push(nomeAmigo.value);
-    if(listaAmigo.textContent == "") {
-       listaAmigo.textContent = nomeAmigo.value;
+    
+    if (listaAmigo.textContent == '') {
+        listaAmigo.textContent = nomeAmigo.value;
     } else {
         listaAmigo.textContent = listaAmigo.textContent + ', ' + nomeAmigo.value;
     }
 
-    nomeAmigo.value = "";
-
-    atualizarLista();
-    atualizarSorteio();
+    nomeAmigo.value = '';
 }
 
 function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione no mínimo 4 nomes!');
+        return;
+    }
+
     embaralha(amigos);
+    
     let sorteio = document.getElementById('lista-sorteio');
 
     for (let i = 0; i < amigos.length; i++) {
         if (i == amigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[1] + ' --> ' + amigos[0] + '<br>';
-        }else {
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] +
+                    '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br/>';
         }
     }
 }
 
-function remover(index){
-    amigos.splice(index, 1);
-    atualizarLista();
-    atualizarSorteio();
-}
-
 function embaralha(lista) {
 
-    let indice = lista.length
-    
-    while(indice) {
-        // atenção para o pós-incremento indice-- 
-        const indiceAleatorio = Math.floor(Math.random() * indice--);
-        [lista[indice], lista[indiceAleatorio]] = 
-            [lista[indiceAleatorio], lista[indice]];
-    }
-}
-
-function atualizarSorteio() {
-    let sorteio = document.getElementById('lista-sorteio');
-    sorteio.innerHTML = '';
-}
-
-function atualizarLista() {
-    let lista = document.getElementById('lista-amigos');
-    lista.innerHTML = '';
-
-    for (let i = 0; i < amigos.length; i++) {
-        // Cria um elemento de parágrafo para cada amigo
-        let paragrafo = document.createElement('p');
-        paragrafo.textContent = amigos[i];
-
-        paragrafo.addEventListener('click', function() {
-            remover(i);
-        });
-        //Adiciona o parágrafo à lista
-        lista.appendChild(paragrafo);
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
 
 function reiniciar() {
     amigos = [];
-    document.getElementById('lista-amigos').innerHTML = '';
-    document.getElementById('lista-sorteio').innerHTML = '';
+    document
+        .getElementById('lista-amigos')
+        .innerHTML = '';
+    document
+        .getElementById('lista-sorteio')
+        .innerHTML = '';
 }
